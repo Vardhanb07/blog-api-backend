ff7264e74eda7965a4d9d948cae4673e1b967d2f
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2025-10-21 20:59:07 +0530

    Add logging functionality

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const passport = require("passport");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
 const postRouter = require("./routes/postRouter");
 const loginRouter = require("./routes/loginRouter");
 require("dotenv").config();
 
 const app = express();
+
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "logs", "access.log"),
+  { flags: "a" }
+);
 
 app.use(
   cors({
@@ -13,6 +21,7 @@ app.use(
     optionsSuccessStatus: 200,
   })
 );
+app.use(morgan("tiny", { stream: accessLogStream }));
 app.use(passport.initialize());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
